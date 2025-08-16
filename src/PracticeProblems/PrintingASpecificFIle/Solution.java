195b440ef26578bb4a29124250f1c6b69ffa8525
AnthomasGit <122173943+AnthomasGit@users.noreply.github.com>
2025-08-16 10:53:02 -0500

    Added Sandbox to gitignore

---
+++ src/PracticeProblems/PrintingASpecificFIle/Solution.java
@@ -0,0 +1,24 @@
+package PracticeProblems.PrintingASpecificFIle;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Which file should have its content printed?");
+        String file = reader.nextLine();
+
+        try (Scanner scanner = new Scanner(Paths.get(file))) {
+
+            while (scanner.hasNextLine()) {
+
+                String row = scanner.nextLine();
+
+                System.out.println(row);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
